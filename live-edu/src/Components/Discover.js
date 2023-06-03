32b7ffa3e50a4f0b3340c996b5aa2cca15fb3d27
ThomasSchou <thom63t4@edu.zealand.dk>
2023-06-03 22:37:11 +0200

    Firestore connection and map

--- live-edu/src/Components/Discover.js
+++ live-edu/src/Components/Discover.js
@@ -1,24 +1,82 @@
 import { useEffect, useState } from "react";
 import { getStreams } from "../Services/IVSApi";
 
+import "../Styles/discover.css";
+import { database } from "../Services/Firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 function Discover() {
 
-    const [channels, setChannels] = useState(null)
+    const [liveChannels, setLiveChannels] = useState(null)
+    const [upcomingChannels, setUpcomingChannels] = useState(null)
 
     useEffect(() => {
-        getStreams()
-            .then(channels => {
-                console.log('List of channels:', channels);
+        const fetchData = async () => {
+            const streamQuery = query(collection(database, "Streams"));
+            const streamSnapshot = await getDocs(streamQuery);
+
+            let live = []
+            let upcoming = []
+
+            streamSnapshot.forEach((doc) => {
+                if(doc.data()) {
+                    if(doc.data().status === "live") { live.push(doc.data())}
+                    if(doc.data().status === "upcoming") { upcoming.push(doc.data())}
+                }
             })
-            .catch(error => {
-                console.error('An error occurred:', error);
-            });
+            setLiveChannels(live)
+            setUpcomingChannels(upcoming)
+        };
+
+        fetchData();
     }, []);
 
-    return (
-        <div className="container">
+    if(!liveChannels || !upcomingChannels) {return <>nada</>}
 
+    const generateLiveItems = () => {
+        return liveChannels.map((item) => {
+            return <div className="discover-item">
+                    <div className="discover-item-image-container"><img className="discover-item-image"></img></div>
+                    <div className="discover-item-info">
+                        <img className="discover-item-portrait"></img>
+                        <div className="discover-item-text">
+                            <div className="discover-item-name">{item.name}</div>
+                            <div className="discover-item-viewers">{item.viewers}</div>
+                            <div className="discover-item-price">{item.price}</div>
+                        </div>
+                    </div>
+                </div>
+        })
+    }
+
+    const generateUpcomingItems = () => {
+        return upcomingChannels.map((item) => {
+            return <div className="discover-item">
+                    <div className="discover-item-image-container"><img className="discover-item-image"></img></div>
+                    <div className="discover-item-info">
+                        <img className="discover-item-portrait"></img>
+                        <div className="discover-item-text">
+                            <div className="discover-item-name">{item.name}</div>
+                            <div className="discover-item-viewers">{item.viewers}</div>
+                            <div className="discover-item-price">{item.price}</div>
+                        </div>
+                    </div>
+                </div>
+        })
+    }
+
+    return (
+        <div className="discover-container">
+            <div className="discover-title">LIVE</div>
+            <div className="discover-grid">
+                {generateLiveItems()}
+                
+            </div>
+            <div className="discover-title">UPCOMING</div>
+            <div className="discover-grid">
+                {generateUpcomingItems()}
+                
+            </div>
         </div>
     );
 }
