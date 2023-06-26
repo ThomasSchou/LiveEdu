1783759240d8938544b3b236122f46f27214de46
ThomasSchou <thom63t4@edu.zealand.dk>
2023-06-26 16:30:14 +0200

    Fixed alot of errors

--- live-edu/src/Components/Quiz.js
+++ live-edu/src/Components/Quiz.js
@@ -1,26 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../Styles/quiz.css";
+import { auth, database } from "../Services/Firebase";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 
-function Quiz(streamId) {
+import { generateRandomString } from "../Helpers/generate";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-  const [isOpen, setIsOpen] = useState(false)
+function Quiz({ streamId }) {
+
+  console.log(streamId)
+  const [data, setData] = useState(null)
+  const [isOpen, setIsOpen] = useState(null)
+  const [user] = useAuthState(auth);
+  
+
+
+  useEffect(() => {
+    if (!streamId) return
+
+    const subscribeToDatabase = () => {
+
+      const unsub = onSnapshot(doc(database, "Quiz", streamId), (doc) => {
+        console.log(doc.data())
+        let tempData
+        setData(doc.data())
+      });
+
+    };
+
+
+    subscribeToDatabase()
+
+  }, [streamId]);
 
   const handleClick = () => {
     setIsOpen(!isOpen)
   }
 
+  if(!data) {return}
+
+  const registerAnswer = async(answer) => {
+    const chatId = generateRandomString(5)
+    let dataRef = doc(database, "Quiz", streamId)
+
+    let chatData = {
+      [user.displayName]:
+      {answer: answer}
+    }
+
+    await updateDoc(dataRef, chatData)
+    setIsOpen(false)
+  }
+
+  const dataValues = Object.keys(data)
+  console.log(dataValues)
   return (
     <div className="quiz-container">
-      <button onClick={handleClick} className="quiz-button">Quiz</button>
+      {dataValues.length !== 0 && <button onClick={handleClick} className="quiz-button">Quiz</button>}
       {isOpen && <div className="quiz-window">
         <div className="quiz-title">Test Quiz</div>
         <div className="quiz-question">
-          <div className="quiz-question-title">Spørgsmål 1</div>
+          <div className="quiz-question-title">{data.name}</div>
           <div className="quiz-question-answers">
-            <div className="quiz-question-answers-item">svar 1</div>
-            <div className="quiz-question-answers-item">svar 2</div>
-            <div className="quiz-question-answers-item">svar 3</div>
-            <div className="quiz-question-answers-item">svar 4</div>
+            <div onClick={() => registerAnswer(data.answer01)} className="quiz-question-answers-item">{data.answer01}</div>
+            <div onClick={() => registerAnswer(data.answer02)} className="quiz-question-answers-item">{data.answer02}</div>
+            <div onClick={() => registerAnswer(data.answer03)} className="quiz-question-answers-item">{data.answer03}</div>
+            <div onClick={() => registerAnswer(data.answer04)} className="quiz-question-answers-item">{data.answer04}</div>
           </div>
         </div>
       </div>}
